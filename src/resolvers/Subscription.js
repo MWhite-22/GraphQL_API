7bab59d1f1a6d75146355c5d006824556b4de3e0
Michael White <mjw291@gmail.com>
2019-02-19 21:22:25 -0500

    Created a newPost Subscription

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,17 @@
+// Still Magic at this time. Need to understand more
+
+
+function newLinkSubscribe(parent, args, context, info){
+	return context.prisma.$subscribe.link({mutation_in: ['CREATED']}).node()
+}
+
+const newLink = {
+	subscribe: newLinkSubscribe,
+	resolve: payload =>{
+		return payload
+	}
+}
+
+module.exports = {
+	newLink
+}
